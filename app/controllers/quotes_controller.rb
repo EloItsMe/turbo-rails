dd56c2383960566ede5e1c219658fd1c8c64aa2a
EloItsMe <elowan.aud@proton.me>
2024-04-29 00:06:30 +0200

    [SETUP] - Demo

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -16,7 +16,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +30,10 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +41,10 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to quotes_url, notice: "Quote was successfully destroyed." }
+    end
   end
 
   private
